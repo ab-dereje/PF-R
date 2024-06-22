bd50fcfb6eda2e1233e1244cd501d3e11fdc4c18
ab-dereje <abudereje09@gmail.com>
2024-06-22 18:20:17 +0300

    first commit

---
+++ src/common/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="bg-[#051426] p-8 flex">
+      <ul className='flex-1 list-none justify-around'>
+        <li className='text-white text-left font-black text-xl'>Abdi D.</li>
+      </ul>
+      <ul className='flex list-none justify-around'>
+        <li><Link className='text-white pr-20 no-underline hover:text-[#29B2FF]' to="/">Home</Link></li>
+        <li><Link className='text-white pr-20 no-underline hover:text-[#29B2FF]' to="/about">About</Link></li>
+        <li><Link className='text-white pr-20 no-underline hover:text-[#29B2FF]' to="/projects">Projects</Link></li>
+        <li><Link className='text-white pr-20 no-underline hover:text-[#29B2FF]' to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='bg-[#051426] h-screen'>
+      <div>
+        <h1 className='text-white'>Hi, I’m Abdi Dereje</h1>
+      </div>
+      <div>
+
+      </div>
+      <h1>Home Page</h1>
+      <p>Welcome to my portfolio!</p>
+    </div>
+  );
+}
+
+export default Home;
